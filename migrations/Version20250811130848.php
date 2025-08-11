2094975fca103ac5432aa874647b7f8ca1008052
MamukaFrance <96849001+MamukaFrance@users.noreply.github.com>
2025-08-11 15:14:25 +0200

    entity sans relation

---
+++ migrations/Version20250811130848.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250811130848 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE etat (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(30) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE lieu (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(30) NOT NULL, rue VARCHAR(50) NOT NULL, latitude DOUBLE PRECISION DEFAULT NULL, longitude DOUBLE PRECISION DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE site (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(30) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sortie (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(30) NOT NULL, infos_sortie VARCHAR(255) NOT NULL, date_heure_debut DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', duree DOUBLE PRECISION DEFAULT NULL, nb_inscriptions_max INT DEFAULT NULL, date_limite_inscription DATE DEFAULT NULL COMMENT \'(DC2Type:date_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, nom VARCHAR(30) NOT NULL, prenom VARCHAR(30) NOT NULL, telephone VARCHAR(10) DEFAULT NULL, administrateur TINYINT(1) DEFAULT 0 NOT NULL, actif TINYINT(1) DEFAULT 1 NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE ville (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(30) NOT NULL, code_postal INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE etat');
+        $this->addSql('DROP TABLE lieu');
+        $this->addSql('DROP TABLE site');
+        $this->addSql('DROP TABLE sortie');
+        $this->addSql('DROP TABLE `user`');
+        $this->addSql('DROP TABLE ville');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
---
+++ src/Entity/Sortie.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SortieRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SortieRepository::class)]
+class Sortie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $infosSortie = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $dateHeureDebut = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $duree = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $nbInscriptionsMax = null;
+
+    #[ORM\Column(type: Types::DATE_IMMUTABLE, nullable: true)]
+    private ?\DateTimeImmutable $dateLimiteInscription = null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getInfosSortie(): ?string
+    {
+        return $this->infosSortie;
+    }
+
+    public function setInfosSortie(string $infosSortie): static
+    {
+        $this->infosSortie = $infosSortie;
+
+        return $this;
+    }
+
+    public function getDateHeureDebut(): ?\DateTimeImmutable
+    {
+        return $this->dateHeureDebut;
+    }
+
+    public function setDateHeureDebut(?\DateTimeImmutable $dateHeureDebut): static
+    {
+        $this->dateHeureDebut = $dateHeureDebut;
+
+        return $this;
+    }
+
+    public function getDuree(): ?float
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(?float $duree): static
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getNbInscriptionsMax(): ?int
+    {
+        return $this->nbInscriptionsMax;
+    }
+
+    public function setNbInscriptionsMax(?int $nbInscriptionsMax): static
+    {
+        $this->nbInscriptionsMax = $nbInscriptionsMax;
+
+        return $this;
+    }
+
+    public function getDateLimiteInscription(): ?\DateTimeImmutable
+    {
+        return $this->dateLimiteInscription;
+    }
+
+    public function setDateLimiteInscription(?\DateTimeImmutable $dateLimiteInscription): static
+    {
+        $this->dateLimiteInscription = $dateLimiteInscription;
+
+        return $this;
+    }
+
+}
